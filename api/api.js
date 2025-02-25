6a5c04025074d64e0ca1ec30f58643b1654d69be
Carlos Moises <carloszeeyy@gmail.com>
2025-02-25 01:29:59 -0300

    o animal aqui esqueceu uma / a mais

--- api/api.js
+++ api/api.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-const URL = "https://backendspotifyclone-production.up.railway.app/";
+const URL = "https://backendspotifyclone-production.up.railway.app";
 
-async function RequestData() {
-  const responseArtists = await axios.get(`${URL}/artists`);
-  const responseSongs = await axios.get(`${URL}/songs`);
-  const artistArray = responseArtists.data;
-  const songsArray = responseSongs.data;
-}
-
-RequestData();
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
